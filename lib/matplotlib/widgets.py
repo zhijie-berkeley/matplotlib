dbc972690beee110e583c7a293826909984ecbf9
Antony Lee <anntzer.lee@gmail.com>
2018-01-27 16:11:14 -0800

    Use Integral and Real in typechecks rather than explicit types.
    See https://docs.python.org/3/library/numbers.html
    https://github.com/numpy/numpy/blob/master/doc/release/1.9.0-notes.rst
    
    When using numpy >=1.9, this will make numpy ints and floats pass the
    relevant typechecks as well.

--- lib/matplotlib/widgets.py
+++ lib/matplotlib/widgets.py
@@ -10,12 +10,13 @@ wide and tall you want your Axes to be to accommodate your widget.
 """
 
 import copy
+from numbers import Integral
 
 import numpy as np
-from matplotlib import rcParams
 
-from .patches import Circle, Rectangle, Ellipse
+from . import rcParams
 from .lines import Line2D
+from .patches import Circle, Rectangle, Ellipse
 from .transforms import blended_transform_factory
 
 
@@ -1459,7 +1460,7 @@ class _SelectorWidget(AxesWidget):
         self.background = None
         self.artists = []
 
-        if isinstance(button, int):
+        if isinstance(button, Integral):
             self.validButtons = [button]
         else:
             self.validButtons = button
