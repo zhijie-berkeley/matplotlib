36fab4cd2136daac35d24116028d3b43f5cc83a5
Antony Lee <anntzer.lee@gmail.com>
2018-05-24 22:52:02 -0700

    Revival of the backend loading refactor.

--- lib/matplotlib/backends/backend_pdf.py
+++ lib/matplotlib/backends/backend_pdf.py
@@ -2582,11 +2582,9 @@ class FigureCanvasPdf(FigureCanvasBase):
                 file.close()
 
 
-class FigureManagerPdf(FigureManagerBase):
-    pass
+FigureManagerPdf = FigureManagerBase
 
 
 @_Backend.export
 class _BackendPdf(_Backend):
     FigureCanvas = FigureCanvasPdf
-    FigureManager = FigureManagerPdf
--- lib/matplotlib/backends/backend_ps.py
+++ lib/matplotlib/backends/backend_ps.py
@@ -1699,8 +1699,7 @@ def pstoeps(tmpfile, bbox=None, rotated=False):
     shutil.move(epsfile, tmpfile)
 
 
-class FigureManagerPS(FigureManagerBase):
-    pass
+FigureManagerPS = FigureManagerBase
 
 
 # The following Python dictionary psDefs contains the entries for the
@@ -1746,4 +1745,3 @@ psDefs = [
 @_Backend.export
 class _BackendPS(_Backend):
     FigureCanvas = FigureCanvasPS
-    FigureManager = FigureManagerPS
